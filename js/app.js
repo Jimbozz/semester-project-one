d535859976ca7c4e4ef0abbd0c9979eea330d1c9
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-12-06 13:57:04 +0100

    added hamburger menu and js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", function() {
+  burger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+
+navLink.forEach(function(item) {
+  item.addEventListener('click', function() {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+})
